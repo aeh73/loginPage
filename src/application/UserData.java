49da6844531398127a5067e7e632a0d664771008
aeh73 <1988wasaverygoodyear@gmail.com>
2023-02-05 20:29:47 +0000

    morecomments
    commented userdata+welcomepage

--- src/application/UserData.java
+++ src/application/UserData.java
@@ -10,36 +10,49 @@ import java.util.Map;
 
 public class UserData {
 
-	private static final String FILE_NAME = "userdata.txt"; // file to store user data
+	// A constant string representing the name of the file to store user data
+	private static final String FILE_NAME = "userdata.txt"; 
 
+	// A method to load user data from the file into a HashMap
 	public static HashMap<String, String> loadData() {
+		  // Create a new HashMap to store the user data
 		  HashMap<String, String> userData = new HashMap<>();
 		  try {
+		    // Create a BufferedReader object to read from the file
 		    BufferedReader reader = new BufferedReader(new FileReader(FILE_NAME));
+		    // Read each line from the file
 		    String line;
 		    while ((line = reader.readLine()) != null) {
+		      // Split the line by comma and store the key-value pair in the HashMap
 		      String[] values = line.split(",");
 		      userData.put(values[0], values[1]);
 		    }
+		    // Close the reader and return the HashMap
 		    reader.close();
 		    return userData;
 		  } catch (IOException e) {
+		    // In case of an error, print the stack trace and return the empty HashMap
 		    e.printStackTrace();
 		    return userData;
 		  }
 		}
 	
+	// A method to save user data from a HashMap to the file
 	public static void saveData(HashMap<String, String> userData) {
 	    try {
+	      // Create a BufferedWriter object to write to the file
 	      BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_NAME));
+	      // Write each key-value pair from the HashMap to a separate line in the file
 	      for (Map.Entry<String, String> entry : userData.entrySet()) {
 	        String line = entry.getKey() + "," + entry.getValue() + "\n";
 	        writer.write(line);
 	      }
+	      // Close the writer
 	      writer.close();
 	    } catch (IOException e) {
+	      // In case of an error, print the stack trace
 	      e.printStackTrace();
 	    }
 	}
 
-}
+} 
